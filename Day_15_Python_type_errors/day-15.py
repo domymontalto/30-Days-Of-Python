83e4b8ca34aa11a17db6a844e1d183047dd94fae
Domenico Montalto <montaltodomenico92@gmail.com>
2026-03-27 21:42:59 -0600

    Day 15

---
+++ Day_15_Python_type_errors/day-15.py
@@ -0,0 +1,32 @@
+# SyntaxError
+# print 'hello world'
+
+# NameError
+# print(age)
+
+# IndexError
+# numbers = [1, 2, 3, 4, 5]
+# numbers[5]
+
+# ModuleNotFoundError
+# import maths
+
+# AttributeError
+# import math
+# math.PI
+
+# KeyError
+# user = {'name':'Asab', 'age':250, 'country':'Finland'}
+# user['county']
+
+# TypeError
+# 4 + '3'
+
+# ImportError
+# from math import power
+
+# ValueError 
+# int('12a')
+
+# ZeroDivisionError
+# 1/0
