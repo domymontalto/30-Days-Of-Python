fffc34c6e7696e4e90d8154239a56a3769942999
Domenico Montalto <montaltodomenico92@gmail.com>
2026-04-01 21:01:53 -0600

    Day 17

---
+++ Day_17_Exception_handling/day-17.py
@@ -0,0 +1,13 @@
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+
+*nordic_countries, es, ru = names
+
+print(f'Unpacked first five countries', nordic_countries)
+
+print()
+
+print(es)
+
+print()
+
+print(ru)
